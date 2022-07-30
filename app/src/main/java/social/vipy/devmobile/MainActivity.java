529db11984eea59a4fa2546107484171c68a4aeb
OakBranches.M <50147318+OakBranches@users.noreply.github.com>
2022-07-30 13:33:16 -0300

    Configurando Binding e arrumando uns botões

--- app/src/main/java/social/vipy/devmobile/MainActivity.java
+++ app/src/main/java/social/vipy/devmobile/MainActivity.java
@@ -1,14 +1,40 @@
 package social.vipy.devmobile;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModel;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import social.vipy.devmobile.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.Entrar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                binding.UsernameInput.setError("Username ou senha Incorretos");
+                binding.PasswordInputInternal.setError("Username ou senha Incorretos");
+
+            }
+        });
+
+        binding.CriarConta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("Navigate","Ir para a outra página");
+            }
+        });
     }
 }
+
+
---
+++ app/src/main/java/social/vipy/devmobile/ViewModels/RegisterViewModel.java
@@ -0,0 +1,10 @@
+package social.vipy.devmobile.ViewModels;
+
+import androidx.lifecycle.ViewModel;
+
+public class RegisterViewModel extends ViewModel {
+    public String username = "";
+    public String email = "";
+    public String name = "";
+    public String password = "";
+}
