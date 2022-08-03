557d3067a545a312021848e4ecd0f2c46a543807
Vinicius Quaresma da Luz <viniciusquaresma1904@gmail.com>
2022-08-03 09:01:36 -0300

    Adicionado import de dependencia

--- app/src/main/java/social/vipy/devmobile/MainActivity.java
+++ app/src/main/java/social/vipy/devmobile/MainActivity.java
@@ -1,6 +1,7 @@
 package social.vipy.devmobile;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Intent;
 import android.os.Bundle;
