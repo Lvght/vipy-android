108f9bb59fba85d217511644c5b048161b45a418
Vinicius Quaresma da Luz <viniciusquaresma1904@gmail.com>
2022-08-03 22:45:47 -0300

    Registro funcionando.

--- app/src/main/java/social/vipy/devmobile/RegisterActivity.java
+++ app/src/main/java/social/vipy/devmobile/RegisterActivity.java
@@ -8,11 +8,20 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import retrofit2.Call;
 import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
+import retrofit2.http.Multipart;
 import social.vipy.devmobile.ViewModels.LoginViewModel;
 import social.vipy.devmobile.ViewModels.RegisterViewModel;
 import social.vipy.devmobile.databinding.ActivityMainBinding;
@@ -38,7 +47,7 @@ public class RegisterActivity extends AppCompatActivity {
                 // TODO Não aceitamos menores de idade.
 
                 binding.birthdayInput.setText(String.format("%02d/%02d/%d", day, month + 1, year));
-                registerViewModel.setBirthday(String.format("%d-%2d-%2d", year, month + 1, day));
+                registerViewModel.setBirthday(String.format("%d-%02d-%02d", year, month + 1, day));
             }, 2000, 0, 1);
 
             dialog.show();
@@ -47,17 +56,17 @@ public class RegisterActivity extends AppCompatActivity {
         binding.criarConta.setOnClickListener(
                 new View.OnClickListener() {
                     public void onClick(View v) {
-                        registerViewModel.setPassword(binding.PasswordInputInternal.getText().toString());
+                        registerViewModel.setPassword(binding.passwordInputInternal.getText().toString());
+                        registerViewModel.setEmail(binding.emailInput.getText().toString());
+                        registerViewModel.setName(binding.displayNameInput.getText().toString());
+                        registerViewModel.setUsername(binding.usernameInput.getText().toString());
 
-                        registerViewModel.setEmail(binding.EmailInput.getText().toString());
+                        // TODO Validações iniciais
+//                        binding.passwordInputInternal.setError("");
+//                        binding.emailInput.setError("");
+//                        binding.displayNameInput.setError("");
 
-                        registerViewModel.setName(binding.NameInput.getText().toString());
-
-                        binding.PasswordInputInternal.setError("");
-                        binding.EmailInput.setError("");
-                        binding.NameInput.setError("");
-
-                        HashMap<String, String> erros = new HashMap<String, String>();
+//                        HashMap<String, String> erros = new HashMap<String, String>();
 
                         // todo fazer a requisição
                         try {
@@ -66,25 +75,83 @@ public class RegisterActivity extends AppCompatActivity {
                                     APIClient.getNonAuthenticatedClient()
                                             .create(VipyAPIClientInterface.class);
 
-                            // TODO Terminar cadastro.
+                            HashMap<String, String> payload = new HashMap<String, String>() {{
+                                put("email", registerViewModel.getEmail());
+                                put("password", registerViewModel.getPassword());
+                                put("display_name", registerViewModel.getName());
+                                put("birthday", registerViewModel.getBirthday());
+                                put("device_name", "Generic DevMobile Android Device");
+                            }};
 
-                            erros.put("name", "NOME INVÁLIDO");
-                            erros.put("email", "EMAIL INVÁLIDO");
-                            erros.put("password", "SENHA INVÁLIDA");
+                            // creates a multipart request
+
+//                            MultipartBody.Builder builder = new MultipartBody.Builder();
+//                            builder.setType(MultipartBody.FORM);
+//                            for (Map.Entry<String, String> entry : payload.entrySet()) {
+//                                builder.addFormDataPart(entry.getKey(), entry.getValue());
+//                            }
+
+                            System.out.println("HSKjashdkjds");
+
+                            MultipartBody.Builder builder =
+                                    new MultipartBody.Builder().setType(MultipartBody.FORM);
+
+                            System.out.println(registerViewModel.getBirthday());
+
+                            builder.addFormDataPart("email", registerViewModel.getEmail())
+                                    .addFormDataPart("password", registerViewModel.getPassword())
+                                    .addFormDataPart("username", registerViewModel.getUsername())
+                                    .addFormDataPart("display_name", registerViewModel.getName())
+                                    .addFormDataPart("birthday", registerViewModel.getBirthday())
+                                    .addFormDataPart("device_name", "Generic DevMobile Android Device");
+
+                            System.out.println("Vai buildarrrrrr");
+                            RequestBody finalizedBody = builder.build();
+
+                            System.out.println("ahdkdhdkshdahdasjkdhkj");
+                            Call<User> call = client.register(finalizedBody);
+
+
+                            call.enqueue(
+                                    new Callback<User>() {
+                                        @Override
+                                        public void onResponse(Call<User> call, Response<User> response) {
+                                            Log.d("tagger", "Código de resposta do registro: " + response.code());
+                                            Log.d("tagger", "Resposta do registro: " + response.body());
+
+                                            if (response.code() == 201) {
+                                                Log.d("tagger", "Novo usuário: " + response.body().toString());
+                                            } else {
+                                                try {
+                                                    Log.d("tagger", "Resposta do errro: " + response.errorBody().string());
+                                                } catch (IOException e) {
+                                                    e.printStackTrace();
+                                                }
+                                            }
+
+                                            // TODO Mudar de tela.
+                                        }
+
+                                        @Override
+                                        public void onFailure(Call<User> call, Throwable t) {
+                                            Log.d("vipyinfo", t.getMessage());
+                                        }
+                                    }
+                            );
 
                         } catch (Exception e) {
 
                         }
 
-                        if (erros.containsKey("name")) {
-                            binding.NameInput.setError(erros.get("name"));
-                        }
-                        if (erros.containsKey("email")) {
-                            binding.EmailInput.setError(erros.get("email"));
-                        }
-                        if (erros.containsKey("password")) {
-                            binding.PasswordInputInternal.setError(erros.get("password"));
-                        }
+//                        if (erros.containsKey("name")) {
+//                            binding.displayNameInput.setError(erros.get("name"));
+//                        }
+//                        if (erros.containsKey("email")) {
+//                            binding.emailInput.setError(erros.get("email"));
+//                        }
+//                        if (erros.containsKey("password")) {
+//                            binding.passwordInputInternal.setError(erros.get("password"));
+//                        }
 
 
                     }
